94482a9f9146802c0c97057c030097d0e6460cb8
Andrey Klimov <anklimov@gmail.com>
2020-10-20 11:49:42 +0300

    -

--- lighthub/lighthub.js
+++ lighthub/lighthub.js
@@ -6,9 +6,10 @@ module.exports = function(RED) {
     // Create our node and event handler
     RED.nodes.createNode(this, config);
 
-    node.on('input', function(msg) {
+    node.on('input', function(msg, send, done)) {
     msg.payload = msg.payload.toLowerCase();
     node.send(msg);
+    done();
 });
 }
 
@@ -16,10 +17,21 @@ module.exports = function(RED) {
 
 
   // Map actionflows with `action in` assocations on scope settings
-  function runtimeMap() {
+  function onStart() {
+  this.status({fill:"red",shape:"ring",text:"disconnected"});
   }
 
 
-  RED.nodes.registerType("lighthub", lighthub);
+
+
+  RED.nodes.registerType("lighthub", lighthub){
+    settings: {
+        config: {
+            value: "red",
+            exportable: true
+        }
+    }
+};
+
   RED.events.on("nodes-started", runtimeMap);
 }
